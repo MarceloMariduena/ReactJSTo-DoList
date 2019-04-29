fe3915339501a7b7b118efa96269fea9ffe82e32
Carbide <carbide@localhost.localdomain>
2019-04-29 16:43:40 -0400

    Updates

---
+++ precache-manifest.d30fe185af62fe38f7334d988c1d4623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69714de40ff0b7b0250941f5777aae6",
+    "url": "/ReactJSTo-DoList/index.html"
+  },
+  {
+    "revision": "153ebf39af794c1c64d8",
+    "url": "/ReactJSTo-DoList/static/css/main.15ff5db9.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/ReactJSTo-DoList/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "153ebf39af794c1c64d8",
+    "url": "/ReactJSTo-DoList/static/js/main.2c8697e5.chunk.js"
+  },
+  {
+    "revision": "ada81522cf6d34966eb0",
+    "url": "/ReactJSTo-DoList/static/js/runtime~main.c164ab7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJSTo-DoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
